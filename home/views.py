f7224acb9d48acecef1337e4c4359671a5f3fc8f
Trishna Roy <trish0991@yahoo.in>
2025-08-08 14:15:26 +0530

    25322

--- home/views.py
+++ home/views.py
@@ -3,6 +3,8 @@ from django.conf import settings # Import project settings.
 
 # View to render the homepage and display the restaurant name.
 def homepage(request):
+
+    restaurant_phone = getattr()
     # Get the restaurant name from settings.py. If not found use the default name "My Restaurant".
     restaurant_name = getattr(settings, 'RESTAURANT_NAME', 'My Restaurant')
     # Render the 'index.html' template inside 'home' folder and passing the restaurant name as context.
