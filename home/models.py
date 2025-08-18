a659cca78acdad94397776fa304ae7919ed65d57
Trishna Roy <trish0991@yahoo.in>
2025-08-18 10:14:54 +0530

    32492

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Feedback(models.Model):
+    comments = models.TextField()
+
+    def __str__(self):
+        return f"Feedback - {self.comments[:30]}..."
