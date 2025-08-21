2278bab291b83bce25cbf2967db914ed9bba6bd4
Trishna Roy <trish0991@yahoo.in>
2025-08-21 18:17:53 +0530

    35118

--- home/models.py
+++ home/models.py
@@ -6,3 +6,12 @@ class Feedback(models.Model):
 
     def __str__(self):
         return f"Feedback - {self.comments[:30]}..."
+
+
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100, help_text='Name of the user.')
+    email = models.EmailField(help_text='Email of the user.')
+    submitted_at = models.DateTimeField(auto_now_add=True, help_text='Timestamp when each contact form was submitted.')
+
+    def __str__(self):
+        return self.name
