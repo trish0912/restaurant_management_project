76cb14caaa831e4d91b48604dcb09302fd1c158a
Trishna Roy <trish0991@yahoo.in>
2025-08-21 11:51:30 +0530

    35121

--- orders/views.py
+++ orders/views.py
@@ -3,4 +3,12 @@ from .models import MenuItem
 
 # Create your views here.
 
+def MenuItem_View(request):
+    try:
+        all_items = MenuItem.objects.all()
+    except Exception as e:
+        all_items = []
+        print(f"Error fetching menu items: {e}")
 
+    return render(request, 'orders/menu_item.html', {'all_items':all_items})
+    
