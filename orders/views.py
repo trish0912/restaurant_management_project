e1fdc69863fdb15c89515864eb3f4153c2a50d79
Trishna Roy <trish0991@yahoo.in>
2025-08-22 11:57:58 +0530

    35117

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import MenuItem
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
 
@@ -12,4 +14,24 @@ def MenuItem_View(request):
         print(f"Error fetching menu items: {e}")
 
     return render(request, 'orders/menu_item.html', {'all_items':all_items})
+
+
+
+# View to retrieve restaurant's menu- Restaurant Menu API
+@api_view(['GET'])
+def menu_api(request):
+    menu = [
+        {
+            'name':'Margherita Pizza',
+            'description':'Classic cheese and tomato pizza',
+            'price':299
+        },
+        {
+            'name':'Pasta Alfredo',
+            'description':'Creamy pasta with garlic and cheese sauce',
+            'price':399
+        }
+    ]
+
+    return Response(menu)
     
