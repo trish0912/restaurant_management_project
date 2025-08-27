b00cd3a9e773085646558023a4c8e9835c2f4b1b
Trishna Roy <trish0991@yahoo.in>
2025-08-27 19:30:03 +0530

    39113

--- orders/urls.py
+++ orders/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('menuitem/', MenuItem_View, name='menuitem'),
-    path('menu/', menu_api, name = 'menu_api'),
+    path('menu/', menu_api, name='menu_api'),
 ]
