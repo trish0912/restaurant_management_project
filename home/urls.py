0c84c0467888dec37b8ddc4a105e8533cfe77739
Trishna Roy <trish0991@yahoo.in>
2025-09-03 18:27:33 +0530

    41559

--- home/urls.py
+++ home/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('history/', about_restaurant, name='history'),
     path('menu/', menu_items, name='menu'),
     path('contact/', contact, name='contact'),
+    path('contactform/', )
     path('reservations/', reservations_view, name='reservations'),
     path('feedback/', feedback_view, name='feedback'),
 ]
